f52249c423e08352b75300b13d86d198aaa0714e
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-12-04 10:08:32 +0530

    ContactUS

--- tests/TC01ContactUs.spec.js
+++ tests/TC01ContactUs.spec.js
@@ -1,23 +1,18 @@
-const {test,expect} = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
-test('Verify contactus functionality',async ({page})=>{
-    //AAA
-    //Arrange
-    //Action 
-    //Assert
-    //tagname[att = value]
-    await page.goto('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
-    await page.locator('input[name="first_name"]').fill('Mahesh')
-    await page.locator('input[name="last_name"]').fill('Aher')
-    await page.locator('input[name="email"]').fill('Mahesh@gmail.com')
-    await page.locator('textarea[name="message"]').fill('I am learning Js')
-    await page.locator('input[type="submit"]').click()
+test('Verify contactus functionality', async ({ page }) => {
+    await page.goto('https://webdriveruniversity.com/Contact-Us/contactus.html')
+    await page.locator('[name="first_name"]').fill('Mahesh')
+    await page.locator('[name="last_name"]').fill('Aher')
+    await page.locator('[placeholder="Email Address"]').fill('mahesh.aher@gmail.com')
+    await page.locator('[name="message"]').fill('I am learning Playwright')
+    await page.locator('[type="submit"]').click()
     await page.waitForTimeout(4000)
-    await expect(page.locator('h1')).toBeVisible()
+    await expect(page.locator('body')).toBeVisible()
     await expect(page.locator('h1')).toHaveText('Thank You for your Message!')
 })
 
-test('Verify contactus functionality invalid email',async ({page})=>{
+test('Verify contactus functionality with invalid email', async ({ page }) => {
     //AAA
     //Arrange
     //Action 
@@ -34,7 +29,7 @@ test('Verify contactus functionality invalid email',async ({page})=>{
     await expect(page.locator('body')).toHaveText('Error: Invalid email address')
 })
 
-test.only('Verify reset button functionality',async({page})=>{
+test('Verify reset button functionality', async ({ page }) => {
     await page.goto('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
     await page.locator('input[name="first_name"]').fill('Mahesh')
     await page.locator('input[name="last_name"]').fill('Aher')
@@ -45,4 +40,5 @@ test.only('Verify reset button functionality',async({page})=>{
     console.log(firstNamefield)
     await expect(firstNamefield).toBe('')
     await expect(firstNamefield).toEqual('')
+    await expect(page.locator('input[name="first_name"]').inputValue()).toBe('')
 })
